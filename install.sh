2139ad1162de6625be663af2041a38092a018a2e
Armin <armin@ant>
2016-10-16 17:34:31 +0200

    fix destination directory location ($dir)

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-dir="${XDG_DATA_HOME:-$HOME/.local/share}/xfce4/colorschemes"
+dir="${XDG_DATA_HOME:-$HOME/.local/share}/xfce4/terminal/colorschemes"
 if [ ! -d ./colorschemes/ ]; then echo "Error: ./colorschemes/ does not exist. Try calling this script from the same directory it resides in."; exit 1; fi
 echo "Copying .theme files to $dir ..."
 mkdir -p "$dir"
