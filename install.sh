0dcde8dd903e2bbd2e50d61af69adbd438e9f9c0
Armin <armin@acid>
2017-04-12 15:49:01 +0200

    use dirname in install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
+a="$(dirname $0)"
+cd "$a"
 dir="${XDG_DATA_HOME:-$HOME/.local/share}/xfce4/terminal/colorschemes"
-if [ ! -d ./colorschemes/ ]; then echo "Error: ./colorschemes/ does not exist. Try calling this script from the same directory it resides in."; exit 1; fi
 echo "Copying .theme files to $dir ..."
 mkdir -p "$dir"
-cp -vn ./colorschemes/*.theme "$dir"
+cp -vin ./colorschemes/*.theme "$dir"
 echo "done."
 
 
